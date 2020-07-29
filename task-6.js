d8ddcca24eaef06916582412b6014ed5ee8b890b
RomanSoroka85 <patriotroma9@gmail.com>
2020-07-29 19:45:49 +0300

    hw1

--- task-6.js
+++ task-6.js
@@ -5,6 +5,9 @@ let total = 0;
 while (input !== 0) {
   input = prompt("введите число");
   console.log(input);
+  if (input === null) {
+    break;
+  }
   total = total + Number(input);
 }
 console.log(total);
